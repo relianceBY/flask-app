5421d22fb24f958eaa6392f58600ec5bdc7320e9
Reliance BY <relianceby@gmail.com>
2021-09-18 17:57:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -59,4 +59,4 @@ if __name__ == "__main__":
     t.start()
 
     context = ('server.crt', 'server.key')
-    app.run(host='0.0.0.0', port=8080, debug=True, ssl_context=context)
+    app.run(host='0.0.0.0', port=443, debug=True, ssl_context=context)
